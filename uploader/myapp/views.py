2d80f766f62254c185b1a44260b4e2b9b071ff0d
mohammad saifuddin <Mohammad.Saifuddin_ext@ltts.com>
2022-12-30 00:01:34 +0530

    adding files

---
+++ uploader/myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from .forms import ImageForm
+from .models import Image
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    form = ImageForm()
+    img = Image.objects.all()
+    return render(request,"myapp/home.html",{'img' : img,"form" : form})
+
+def delete_image(request,id):
+    instance = Image.objects.get(id=id)
+    instance.delete()
+    return redirect("/")
